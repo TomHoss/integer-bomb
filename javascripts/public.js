bccfbf03ed61698f019aaf114e18f110575ab1b9
Tom Hosford <tom.hosford@gmail.com>
2013-07-28 19:25:12 -0400

    intense refactor

--- javascripts/public.js
+++ javascripts/public.js
@@ -1,5 +1,9 @@
-//random integer to string transition/convergence algorithm
-//@2013 Thomas Hosford, New York NY
+/*
+AMDG
+
+Random Integer to String Transition/Convergence Algorithm
+Copyright © 2013 Thomas Hosford, New York, NY.
+*/
 
 $(document).ready(function() {
   var body, typeWriter, timesCalled;
@@ -7,21 +11,17 @@ $(document).ready(function() {
 
   $("body").fadeIn(1000);
 
-  typeWriter = setInterval(randomizeBody, 50)
+  randomizer = setInterval(randomizeBody, 50)
 
   setTimeout(function() {
-    clearInterval(typeWriter)
-    $("#body").empty()
+    clearInterval(randomizer)
+    $("#backdrop").empty()
     //add work, essays
     //moving fullbleeds of NY
     //audio on/off button
   }, 2000)
 })
 
-//higher ws prob for core content
-
-//replace the body with 7000 random numbers every 50 ms, for 2000 ms
-var whiteSpaceProbability = 0;
 var randomIntOrSpace = function(whiteSpaceProbability) {
     if (Math.random() < whiteSpaceProbability) {
       return " "
@@ -30,68 +30,84 @@ var randomIntOrSpace = function(whiteSpaceProbability) {
     }
   }
 
-var timesCalled = 0;
-var randomizeBody = function() {
-  var randomInt;
-  var body_content = "";
-  var intro_content;
-
-  timesCalled++
-
-  if (timesCalled > 35) {
-    whiteSpaceProbability = .9
-  } else if (timesCalled > 30) {
-    whiteSpaceProbability = .8
+//Increase whitespace probability over time
+var whiteSpaceProbability = 0;
+var test = null;
+//TODO: Find out why this breaks if I remove the breaks
+var updateWhiteSpaceProbability = function() {
+  switch(timesCalled)
+  {
+    case 35:
+      whiteSpaceProbability = .9
+      break
+    case 30:
+      whiteSpaceProbability = .8
+      break
+    case 20:
+      whiteSpaceProbability = .4
+      break
+    case 10:
+      whiteSpaceProbability = .1
+      break
   }
-  else if (timesCalled > 20) {
-    whiteSpaceProbability = .4
-  }
-  else if (timesCalled > 10) {
-    whiteSpaceProbability = .1
-  }
-
-  for (i = 0; i < 7000; i++) {
-    body_content += randomIntOrSpace(whiteSpaceProbability)
-  }
-
-  intro_content = randomizeIntro(whiteSpaceProbability)
-
-  $("#body").html(body_content)
-  $("#intro").html(intro_content)
 }
 
-var wordCharProbability = .1;
+var timesCalled = 0; //will be called 40 times total
+var randomizeBody = function() {
+  timesCalled++
 
+  updateWhiteSpaceProbability()
+  console.log(whiteSpaceProbability)
+  console.log(test)
+
+  randomizeIntro()
+  randomizeBackdrop()
+}
+
+//Backdrop randomization
+var randomizeBackdrop = function() {
+  var backdropContent = ""
+  for (i = 0; i < 7000; i++) {
+    backdropContent += randomIntOrSpace(whiteSpaceProbability)
+  }
+  $("#backdrop").html(backdropContent)
+}
+
+//Intro randomization
 var finalIntroContentChars = ("ThomasHosford-SoftwareEngineer").split("")
 var zoneLength = 180 / finalIntroContentChars.length // works out to 6
 var zoneCharShown = []
 for (i = 0; i < 30; i++) {
   zoneCharShown.concat(false)
 }
-var intro_content = []
+
+var introContent = []
 var randomizeIntro = function() {
+  var wordCharProbability = .1;
+
   console.log(timesCalled)
-  console.log(intro_content)
-  var final_intro_content = ("ThomasHosford-SoftwareEngineer")
+  console.log(introContent)
+  var finalIntroContent = ("ThomasHosford-SoftwareEngineer")
 
   //TODO: get exact width
   for (i = 0; i < 180; i++) {
     //populate data on first try
-    if (typeof intro_content[i] == "undefined") {
-      intro_content[i] = randomIntOrSpace()
-    } //leave the char alone if it's already a letter character
-    else if (intro_content[i].match(/[A-Za-z\-]/) === null) {
-      currentZone = parseInt(i/zoneLength)
+    if (typeof introContent[i] == "undefined") {
+      introContent[i] = randomIntOrSpace(whiteSpaceProbability)
+    //leave the char alone if it's already a letter character
+    } else if (introContent[i].match(/[A-Za-z\-]/) === null) {
+      currentZone = parseInt(i / zoneLength)
 
-      if ((Math.random() > wordCharProbability) || (zoneCharShown[currentZone])) { //Don't put another letter in if the zone has been filled
-        intro_content[i] = randomIntOrSpace(whiteSpaceProbability + .1)
+      //Never add a letter if the zone already has a letter in it
+      if ((zoneCharShown[currentZone]) || (Math.random() > wordCharProbability)) {
+        introContent[i] = randomIntOrSpace(whiteSpaceProbability + .1)
       } else {
-        intro_content[i] = finalIntroContentChars[currentZone]
+        introContent[i] = finalIntroContentChars[currentZone]
         zoneCharShown[currentZone] = true
-        console.log("introducing " + intro_content[i])
+        console.log("introducing " + introContent[i])
       }
     }
   }
 
-  return intro_content.join("")
+  $("#intro").html(introContent.join(""))
 }
